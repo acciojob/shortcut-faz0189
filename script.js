64b7e81933ab869d1a28cdabf3d43a1552df16ca
faz0189 <128984518+faz0189@users.noreply.github.com>
2025-03-11 13:11:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function shortcut(s1, s2) {
-  // your code here
+ if(!s1 || !s2){
+	 return " ";
+	 return s1[0,1] + s2[0,1]
+ }
+	
 }
 
-// Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
